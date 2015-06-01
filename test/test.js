8a642b8719023d83eebc2c83a605d61509c032c8
alfred sang <i5ting@126.com>
2015-06-01 17:19:54 +0800

    refact

--- test/test.js
+++ test/test.js
@@ -1,14 +1,10 @@
-require('../db');
+require('./db');
 
-
-Meeting = require('./Meeting');
- 
+var Meeting = require('./Meeting');
 
 Meeting.create({"username":"sss","password":"password"},function(err, user){
   console.log(user);
 });
-
-
 
 Meeting.delete({"username":"sss","password":"password"},function(err, user){
   console.log(user);
